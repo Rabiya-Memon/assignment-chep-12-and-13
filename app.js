52c01e7eea249a976b63497ab1f2cc22c9108aa5
Rabiya Memon <rabiaghulam56@gmail.com>
2024-12-16 17:07:37 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,89 @@
+//question no 1
+// let num = prompt("enter a number please \n number \n uppercase \n lowercase");
+// if(num >= 48 && num <= 54){
+//     console.log("its a number");
+// }
+// else if(num >= 65 && num <= 90){
+//     console.log("its a uppercase");
+// }
+// else if(num >=97 && num <=122){
+//     console.log("its a lower case");
+// }
+// else{
+//     console.log("invalid number");
+// }
+
+
+//question no 2
+// let num1 = prompt("enter a number");
+// let num2 = prompt("enter a number");
+// if(num1<num2){
+// console.log("its larger number");
+// }
+// else if(num1<num2){
+//     console.log("its smaller");
+// }
+// else if(num1 == num2){
+//     console.log(" correct its equal");
+// }
+
+
+//question no 3
+// let input = prompt("enter a number");
+// if(input > 0){
+//     console.log("its positive integer");
+// }
+// else if(input < 0){
+//     console.log("its negitive integer");
+// }
+// else{
+//     console.log("its zero");
+// }
+
+//question no 4
+// let character =prompt("enter a character");
+// if(character == a || character == A || character == e || character == E || character == i || character == I || character == o || character == O || character == u || character == U){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+//quetion no 5
+// let password = "saylani";
+// let user = prompt("enter a password");
+// if(user == " "){
+//     console.log("lease enter password");
+// }
+// else if(password == user){
+//     console.log("password is correct");
+// }
+// else if(password != user){
+// console.log("try again password is incorrect");
+// }
+
+//question no 6
+// let greeting;
+// let hour = 13;
+// if(hour < 18){
+// greeting = "good day";
+// }
+// else{
+//     greeting = "good evening";
+// }
+
+
+//question no 7
+// let time = 1900;
+// if(time >= 0o00 && time < 1200){
+//     console.log("Good morning!");
+// }
+// else if(time >= 1200 && time < 1700){
+//     console.log("Good afternoon!");
+// }
+// else if(time >= 1700 && time < 2100){
+//     console.log("Good evening!");
+// }
+// else if(time >= 2100 && time <= 2359){
+//     console.log("Good night!");
+// }
